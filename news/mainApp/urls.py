273cb170629fec44641c83abc19cbcb092da5cda
nextalgon <nextalgon6769@gmail.com>
2021-06-12 02:25:32 +0500

    davay bolla oz qoldi!!

--- news/mainApp/urls.py
+++ news/mainApp/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from mainApp.views import MahsulotView
+from mainApp.views import TopicView
 
 
 urlpatterns = [
 
     path("", views.index, name="index"),
-    path("mahsulot/<int:pk>/", MahsulotView.as_view()),
+    path("topic/<int:pk>/", TopicView.as_view(), name='davomi'),
     path('category/<int:category_id>/', views.category, name="category"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/mainApp/views.py
+++ news/mainApp/views.py
@@ -3,22 +3,23 @@ from .models import Category, NewTopic
 from django.core.paginator import Paginator
 from django.views.generic import DetailView
 
+
 def index(request):
     topics = NewTopic.objects.all()
     categories = Category.objects.all()
-    return render(request, 'mainApp/wrapper.html', {'topics': topics, 'categories': categories})
+    return render(request, 'mainApp/home.html', {'topics': topics, 'categories': categories})
 
 
 def category(request, category_id):
     page = request.GET.get('page')
-    toifa = Category.objects.get(pk=category_id)
-    all_topics = NewTopic.objects.filter(toifasi=toifa)
-    paginator = Paginator(all_topics, 6)
+    categ = Category.objects.get(pk=category_id)
+    all_topics = NewTopic.objects.filter(category=categ)
+    paginator = Paginator(all_topics, 2)
     topics = paginator.get_page(page)
-    categoriess = Category.objects.all()
-    return render(request, 'news/wrapper.html', {'topices': topics, 'categoriess': categoriess})
+    categories = Category.objects.all()
+    return render(request, 'mainApp/home.html', {'categories': categories, 'topics': topics})
 
 
-class MahsulotView(DetailView):
+class TopicView(DetailView):
     model = NewTopic
-    template_name = "news/Newtopic.html"
+    template_name = "mainApp/topic.html"
